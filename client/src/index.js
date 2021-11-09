0082d09352400c9d82c5b28b479653bcc05088d0
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-09 08:23:05 -0600

    all components working; no stretch yet

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
+);
+
+/**
+ * gives access to history, match and location when we "wrap" the <App /> within the <Router> tags
+ */
